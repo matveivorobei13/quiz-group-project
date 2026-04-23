d696efeff154c1d8137c506e8e9b2b89d7ef2c42
matveivorobei13 <matvejmuha64@gmail.com>
2026-04-23 11:33:53 +0300

    added new javaScript file with quiz object

---
+++ script.js
@@ -0,0 +1,32 @@
+const quiz = [
+  {
+    question: "Столица Франции?",
+    answers: ["Берлин", "Париж", "Мадрид", "Рим"],
+    correct: 1
+  },
+  {
+    question: "2 + 2 = ?",
+    answers: ["3", "4", "5", "6"],
+    correct: 1
+  },
+  {
+    question: "Цвет неба?",
+    answers: ["Зелёный", "Синий", "Красный", "Жёлтый"],
+    correct: 1
+  },
+  {
+    question: "HTML — это?",
+    answers: ["Язык", "База", "Сервер", "ОС"],
+    correct: 0
+  },
+  {
+    question: "JS — это?",
+    answers: ["Стиль", "Язык", "Браузер", "Файл"],
+    correct: 1
+  },
+  {
+    question: "CSS нужен для?",
+    answers: ["Логики", "Стилей", "Базы", "Сервера"],
+    correct: 1
+  }
+];
